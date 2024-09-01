f83f9adcaf6f550ec14d887ece6da0248879c4a7
Narottam <narottamk077@gmail.com>
2024-09-01 15:19:49 +0530

    updated some mistake

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+# us_visa/main.py
+
 from us_visa.components.data_ingestion import main
 
 main()
--- us_visa /components/data_ingestion.py
+++ us_visa /components/data_ingestion.py
@@ -1,4 +1,2 @@
-from us_visa.components.data_ingestion import main
-
-if __name__ == "__main__":
-    main()
+def main():
+    print('Hi')
